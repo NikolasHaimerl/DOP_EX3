7eddb4fa4d00811d4f7c4a5c2064a1e4fd45b835
NikolasHaimerl <nikolas.haimerl@outlook.com>
2020-12-27 15:06:35 +0100

    Web Scraper done

--- src/data_preparation/Response_Variable.py
+++ src/data_preparation/Response_Variable.py
@@ -1,41 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-#
-# year=2019
-# url_base=f"https://www.numbeo.com/quality-of-life/rankings.jsp?title={year}&displayColumn=0"
-# page=requests.get(url_base)
-# soup=BeautifulSoup(page.content,"html.parser")
-# tb=soup.find(id="t2")
-# header=tb.find_all_next("th")
-# headings=[]
-# for head in header:
-#     headings.extend(head.div.contents)
-# print(headings)
-#
-# body=tb.find_next("tbody").find_all("tr")
-# ranks=[]
-# city=[]
-# countries=[]
-# qol=[]
-# rank = 1
-# for row in body:
-#     content=row.find_all("td")
-#     ranks.append(rank)
-#     city.extend(content[1])
-#     qol.extend(content[2])
-#     rank+=1
-# print(ranks)
-# print(city)
-# print(qol)
-# tupls=list(zip(ranks,city,qol))
-# df=pd.DataFrame(tupls,columns=headings)
-# cities=df["City"].map(lambda x:x.split(",")[0])
-# countries=df["City"].map(lambda x:x.split(",")[1])
-# df["City"]=cities
-# df["Country"]=countries
-# print(df)
-
 DEFAULT_YEAR=2020
 class ReponseVariableScraper:
 
@@ -49,12 +14,19 @@ class ReponseVariableScraper:
         if not path: path = f"../data/Quality_of_life_{year}"
         self.year = year
         self.url_base = f"https://www.numbeo.com/quality-of-life/rankings.jsp?title={year}&displayColumn=0"
+        if year==2012 or year ==2013:self.url_base = f"https://www.numbeo.com/quality-of-life/rankings.jsp?title={year}-Q1&displayColumn=0"
+
         self.__process_content()
         self.__save_data(path)
 
     def get_year(self,year,path=None):
         if not path: path = f"../data/Quality_of_life_{year}"
-        return pd.read_csv(path,header=0)
+        try:
+            df=pd.read_csv(path,header=0)
+        except(FileNotFoundError):
+            self.scrape_year(year)
+            df=self.get_year(year)
+        return df
 
     def __process_content(self):
         page = requests.get(self.url_base)
@@ -74,6 +46,7 @@ class ReponseVariableScraper:
             ranks.append(rank)
             city.extend(content[1])
             qol.extend(content[2])
+            rank+=1
         tupls = list(zip(ranks, city, qol))
         df = pd.DataFrame(tupls, columns=headings)
         cities = df["City"].map(lambda x: x.split(",")[0])
@@ -86,7 +59,9 @@ class ReponseVariableScraper:
         self.data[self.year].to_csv(path,header=True)
 
 
-if __name__=="__main___":
+if __name__=="__main__":
     scraper=ReponseVariableScraper()
-    scraper.scrape_year(2012)
+    for year in range(2012,2020):
+        scraper.scrape_year(year)
+
 
